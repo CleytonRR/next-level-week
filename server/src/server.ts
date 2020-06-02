6ec705030052ea19415647e78dc942abd386d2ae
cleytonRR <cleytonrodrigues69@live.com>
2020-06-02 20:19:49 -0300

    Clean application

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { request, response } from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  response.json(["Diego", "Cleiton", "Robson", "Daniel"]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
