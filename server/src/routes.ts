bb5c5661e1dcaae7a71069885572d54cf16d81ab
cleytonRR <cleytonrodrigues69@live.com>
2020-06-02 22:15:03 -0300

    Created routes for create and list points

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,56 @@
 import express from "express";
 const routes = express.Router();
+import knex from "./database/connection";
 
-routes.get("/", (request, response) => {
-  return response.json({ message: "Teste" });
+routes.get("/items", async (request, response) => {
+  const items = await knex("items").select("*");
+  const serializedItems = items.map((item) => {
+    return {
+      id: item.id,
+      title: item.name,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+  return response.json(serializedItems);
+});
+
+routes.post("/points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  const insertIds = await trx("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertIds[0];
+
+  const pointItems = items.map((item_id: Number) => {
+    return {
+      item_id,
+      point_id,
+    };
+  });
+
+  await trx("point_items").insert(pointItems);
+
+  return response.json({ sucess: true });
 });
 
 export default routes;
